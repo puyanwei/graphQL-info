8ab280c6c08925dacceaff8814799992da384bad
Puyan Wei <puyanwei@gmail.com>
2018-09-08 13:01:13 +0100

    Added setup for express-graphQL

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const graphQLHTTP = require("express-graphql");
 
 const app = express();
+
+// Middleware that hands off the request to express-graphQL to handle when a request is made to /graphql
+app.use("/graphql", graphQLHTTP({}));
+
+// Schema needs to be passed into graphQL, which tells express-graphQL how the data is formatted
 
 app.listen(1337, () => {
     console.log(`listening to requests on port 1337`);
